0803ab9dfe465ee767e4a0f071f6cd4be95bb91d
Tanner Woodworth <twoodworth2014@gmail.com>
2017-03-08 14:48:06 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var myApp = angular.module('flashcardApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider){
+
+});
+
+myApp.controller('mainController', ['$scope', function($scope) {
+
+}]);
