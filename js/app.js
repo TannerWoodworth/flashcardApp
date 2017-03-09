9a56fa17e261a33e5da59611a532e7cce4349c8b
Tanner Woodworth <twoodworth2014@gmail.com>
2017-03-09 18:02:55 -0500

    started adding services

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var flashcardApp = angular.module('flashcardApp', ['ngRoute']);
 
 // ROUTES
+/////////////////////////////////////////////////
 
 flashcardApp.config(function ($routeProvider, $locationProvider){
 
@@ -29,16 +30,48 @@ $routeProvider
 
 });
 
-// CONTROLLERS
+// SERVICES
+/////////////////////////////////////////////////
 
-flashcardApp.controller('splashController', ['$scope', function($scope) {
+flashcardApp.service('cardService', function() {
+   
+   this.savedCards = [
+		{
+			front: 'Card Front 1',
+			back: 'Card Back 1',
+			id: 1
+		},
+		{
+			front: 'Card Front 2',
+			back: 'Card Back 2',
+			id: 2
+		},
+		{
+			front: 'Card Front 3',
+			back: 'Card Back 3',
+			id: 3
+		},
+		{
+			front: 'Card Front 4',
+			back: 'Card Back 4',
+			id: 4
+		}];
+  
+});
+
+// CONTROLLERS
+/////////////////////////////////////////////////
+
+flashcardApp.controller('splashController', ['$scope', 'cardService', function($scope, cardService) {
 
 // IMPORTING JSON OBJECT
-
+	$scope.importCards = function() {
+		console.log('Import!');
+	}
 
 }]);
 
-flashcardApp.controller('createController', ['$scope', function($scope) {
+flashcardApp.controller('createController', ['$scope', 'cardService', function($scope, cardService) {
 
 $scope.currentFront = 'Card Front Text';
 $scope.currentBack = 'Card Back Text';
@@ -63,7 +96,7 @@ $scope.savedCards = [
 			front: 'Card Front 4',
 			back: 'Card Back 4',
 			id: 4
-		}]
+		}];
 
 	$scope.addNewCard = function(savedCards) {
 		console.log('Front: ' + $scope.currentFront);
@@ -72,7 +105,9 @@ $scope.savedCards = [
 
 }]);
 
-flashcardApp.controller('saveController', ['$scope', function($scope) {
+flashcardApp.controller('saveController', ['$scope', 'cardService', function($scope, cardService) {
+
+	$scope.cardSetName = 'Ex. Data structures and algorithms';
 
 // PARSING JSON OBJECT
 
@@ -80,14 +115,19 @@ flashcardApp.controller('saveController', ['$scope', function($scope) {
 
 // EXPORTING JSON OBJECT
 
+	$scope.exportCards = function() {
+		console.log('Export!');
+		console.log($scope.cardSetName);
+	}
 
 }]);
 
-flashcardApp.controller('studyController', ['$scope', function($scope) {
+flashcardApp.controller('studyController', ['$scope', 'cardService', function($scope, cardService) {
 
 }]); 
 
 // DIRECTIVES
+/////////////////////////////////////////////////
 
 flashcardApp.directive("finishedCard", function() {
 	return {
